7b04c90a3fda3d80cab4abc3c5c5b9736f3569a3
LI JIAHAO <lijiahao99131@gmail.com>
2018-04-04 19:08:46 +0800

    move to sampleplugin.go

--- sampleplugin.go
+++ sampleplugin.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	. "github.com/cool2645/kotori-ng-sampleplugin/sampleplugin"
+	"github.com/cool2645/kotori-ng-sampleplugin/handler"
 	. "github.com/cool2645/kotori-ng/kotoriplugin"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 )
+
+type SamplePlugin struct {
+	Name    string
+	Version string
+}
+
+func (p *SamplePlugin) GetName() string {
+	return p.Name
+}
+
+func (p *SamplePlugin) GetVersion() string {
+	return p.Version
+}
+
+func (p *SamplePlugin) RegRouter(r *mux.Router) {
+	r.Methods("GET").Path("").HandlerFunc(handler.Pong)
+}
+
+func (p *SamplePlugin) InitDB(db *gorm.DB) {
+	return
+}
+
 
 var PluginInstance Plugin = &SamplePlugin{
 	Name:    "Sample Plugin",
