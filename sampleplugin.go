aa48ddf6cb757df4606e83b91875dea58bee8783
LI JIAHAO <lijiahao99131@gmail.com>
2018-04-05 00:57:04 +0800

    add docs

--- sampleplugin.go
+++ sampleplugin.go
@@ -23,16 +23,23 @@ func (p *SamplePlugin) GetVersion() string {
 }
 
 func (p *SamplePlugin) LoadConfig() error  {
+	// Here you load your plugin config file
+	// A recommended place for your config file to locate is the conf.d folder
 	_, err := toml.DecodeFile("conf.d/kotori-ng-sampleplugin.toml", &GlobCfg)
 	return err
 }
 
 func (p *SamplePlugin) RegRouter(r *mux.Router) error {
+	// Here you can configure your route
+	// your "" will be /api/kotori-ng-sampleplugin (<-This is the plugin name)
+	// while "/" will be /api/kotori-ng-sampleplugin/ (and so on)
 	r.Methods("GET").Path("").HandlerFunc(handler.Pong)
 	return nil
 }
 
 func (p *SamplePlugin) InitDB(db *gorm.DB) error {
+	// Here you can do some migrations, such as
+	// db.AutoMigrate(SomeModel{})
 	return nil
 }
 
